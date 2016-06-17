8a008efbd75a173ea63d55855d359d1130621ba4
levyc <491796258@qq.com>
2016-06-17 09:55:15 +0800

    update

--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -11,7 +11,21 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class ClientMain {
+	private String host;
+	private int port;
+
+	public ClientMain(String host, int port) {
+		this.host = host;
+		this.port = port;
+	}
+
+	private boolean stop = false;
+
 	public static void main(String[] args) throws IOException {
+		new ClientMain(args[0], Integer.parseInt(args[1])).run();
+	}
+
+	public void run() throws IOException {
 		EventLoopGroup worker = new NioEventLoopGroup();
 		Bootstrap bootstrap = new Bootstrap();
 		bootstrap.group(worker);
@@ -19,13 +33,15 @@ public class ClientMain {
 		bootstrap.handler(new ClientIniter());
 
 		try {
-			Channel channel = bootstrap.connect("127.0.0.1", 8888).sync()
-					.channel();
-			while (true) {
+			Channel channel = bootstrap.connect(host, port).sync().channel();
+			while (!stop) {
 				BufferedReader reader = new BufferedReader(
 						new InputStreamReader(System.in));
 				String input = reader.readLine();
 				if (input != null) {
+					if ("quit".equals(input)) {
+						System.exit(0);
+					}
 					channel.writeAndFlush(input);
 				}
 			}
@@ -33,6 +49,5 @@ public class ClientMain {
 			e.printStackTrace();
 			System.exit(1);
 		}
-
 	}
 }
--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -8,7 +8,18 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class ServerMain {
+
+	private int port;
+
+	public ServerMain(int port) {
+		this.port = port;
+	}
+
 	public static void main(String[] args) {
+		new ServerMain(Integer.parseInt(args[0])).run();
+	}
+
+	public void run() {
 		EventLoopGroup acceptor = new NioEventLoopGroup();
 		EventLoopGroup worker = new NioEventLoopGroup();
 		ServerBootstrap bootstrap = new ServerBootstrap();
@@ -17,8 +28,8 @@ public class ServerMain {
 		bootstrap.channel(NioServerSocketChannel.class);
 		bootstrap.childHandler(new ServerIniterHandler());
 		try {
-			Channel channel = bootstrap.bind(8888).sync().channel();
-			System.out.println("server strart running");
+			Channel channel = bootstrap.bind(port).sync().channel();
+			System.out.println("server strart running in port:" + port);
 			channel.closeFuture().sync();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
